983f4f4d12a52f72c1e390e607231e8950aff676
Aditya kumar <adityakumar8279@email.com>
2024-09-02 23:51:29 +0530

    add routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './assets/pages/Home'
 import './App.css';
+import SingUp from './assets/pages/SingUp';
+import SingIn from './assets/pages/SingIn';
+import Profile from './assets/pages/Profile';
+import About from './assets/pages/About';
 
 function App() {
   
   return (
-    <>
-    <h1>real</h1>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/about' element={<About/>}></Route>
+      <Route path='/profile' element={<Profile/>}></Route>
+      <Route path='/sing-up' element={<SingUp/>}></Route>
+      <Route path='/sing-in' element={<SingIn/>}></Route>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/assets/pages/SingIn.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+function SingIn(){
+    return(
+        <>
+        <h1>SingIn</h1>
+        </>
+    );
+}
+
+export default SingIn;
