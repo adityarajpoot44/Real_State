4b5db9887bab920457474deb573d595d0e2c6e97
Aditya kumar <adityakumar8279@email.com>
2024-09-03 14:27:31 +0530

    create the server

---
+++ api/index.js
@@ -0,0 +1,10 @@
+// import express from "express";
+const express = require('express')
+
+const app = express();
+
+const port = 3000;
+
+app.listen(port,()=>{
+    console.log(`server is ready ${port}+ !`);
+})
