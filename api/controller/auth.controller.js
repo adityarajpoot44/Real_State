0a8997491fd04744231e60172d806177674fc49c
adityarajpoot44 <adityakumar8279@gmail.com>
2024-09-07 13:13:50 +0530

    remove the real pass

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -4,8 +4,7 @@ import bcryptjs from "bcryptjs";
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashPassword = bcryptjs.hashSync(password, 10);
-  const realpass = password;
-  const newUser = new User({ username, email, password: hashPassword ,realpass});
+  const newUser = new User({ username, email, password: hashPassword});
   try {
     await newUser.save();
     res.send("user created");
